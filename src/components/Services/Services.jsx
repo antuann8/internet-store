3c60ea15c14eae47e9e2ee9144f9b16bd4474ae9
antuann8 <kashirin.antosha@mail.ru>
2024-06-07 13:58:18 +0300

    services done

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -4,11 +4,11 @@ import { servicesData } from "./../../constants/services";
 export default function Services() {
   return (
     <div className={styles.container}>
-      {servicesData.map((item) => (
-        <div>
-          <img src={item.icon} />
-          <h2>{item.name}</h2>
-          <span>{item.text}</span>
+      {servicesData.map((item, index) => (
+        <div className={styles.services} key={index}>
+          <img className={styles.img} src={item.icon} />
+          <h2 className={styles.header}>{item.name}</h2>
+          <span className={styles.text}>{item.text}</span>
         </div>
       ))}
     </div>
