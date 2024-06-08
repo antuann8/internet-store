9ede9222bd922961198f71d23ca564a66cd03d63
antuann8 <kashirin.antosha@mail.ru>
2024-06-08 11:13:53 +0300

    reviews done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Discount from "./components/Discount/Discount";
 import Header from "./components/Header/Header";
 import Mobile from "./components/Mobile/Mobile";
 import News from "./components/News/News";
+import Reviews from "./components/Reviews/Reviews";
 import Services from "./components/Services/Services";
 import Slider from "./components/Slider/Slider";
 
@@ -17,6 +18,7 @@ function App() {
       <Clock />
       <Discount />
       <News />
+      <Reviews />
     </div>
   );
 }
